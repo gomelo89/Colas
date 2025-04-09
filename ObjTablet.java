e9f2330476d13dc7656298bc9d111c6106642049
Your Name <you@example.com>
2025-04-09 17:23:05 -0500

    commit init

---
+++ ObjTablet.java
@@ -0,0 +1,63 @@
+public class ObjTablet {
+    private String serial;
+    private int tamaño;
+    private String marca;
+    private double precio;
+    private String NombreUsuario;
+    private boolean disponible;
+
+    public ObjTablet() {
+    }
+
+    public String getSerial() {
+        return serial;
+    }
+
+    public void setSerial(String serial) {
+        this.serial = serial;
+    }
+
+    public int getTamaño() {
+        return tamaño;
+    }
+
+    public void setTamaño(int tamaño) {
+        this.tamaño = tamaño;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getNombreUsuario() {
+        return NombreUsuario;
+    }
+
+    public void setNombreUsuario(String NombreUsuario) {
+        this.NombreUsuario = NombreUsuario;
+    }
+
+    public boolean getDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+    
+
+    
+}
+
